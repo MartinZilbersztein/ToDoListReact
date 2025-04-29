a6674db0b0cc8b2df01181e3c2b47ae601b82306
Android Studio <devandroid@ort>
2025-04-29 09:34:17 -0300

    No permite null :)2

--- src/Anadir.jsx
+++ src/Anadir.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Anadir = ({setTareas,tareas}) => {
-
+  const [p, setP] = useState();
   const agregarTarea = ()=>{
     //obtener valor del input
     let nombreTarea = document.getElementById('tarea').value;
     
+    if(nombreTarea.trim() !== "") {
     //crear el objeto nuevo
     const tarea = {
       id: tareas.length+1,
@@ -15,10 +16,15 @@ export const Anadir = ({setTareas,tareas}) => {
       fechaRealizacion:"N/A"
     }
 
-    //agrego al array de tareas    
-    let aux = [...tareas,tarea];    
+    //agrego al array de tareas   
+    
+      let aux = [...tareas,tarea];    
     setTareas(aux);
 
+  }
+  else{
+    setP("Por favor, ingrese una tarea válida");
+  }
     //borrar el contenido del formulario
     document.getElementById('tarea').value = "";
   }
@@ -28,6 +34,7 @@ export const Anadir = ({setTareas,tareas}) => {
         <h1>To-do</h1>
         <input className="agregar" type="text" id="tarea" />
         <input onClick={()=>agregarTarea()} className="enviar" type="button" value="Enviar" />
+        <p>{p}</p>
     </>
   )
 }
