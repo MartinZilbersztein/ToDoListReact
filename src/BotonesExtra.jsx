980441ba723a298b863e7a0e2fca22d93b5c3d02
Android Studio <devandroid@ort>
2025-04-29 08:17:45 -0300

    Calcular tarea más rápida

--- src/BotonesExtra.jsx
+++ src/BotonesExtra.jsx
@@ -6,10 +6,25 @@ export const BotonesExtra = ({lista, setTareas}) => {
     aux = [];
     setTareas(aux);
   }
+  let masRapida = Number.MAX_VALUE;
+  let idMasRapida = '';
+  const calcularTarea = () =>{
+
+    lista.map(item => {
+      if((item.fechaRealizacion - item.fechaCreacion)<masRapida)
+      {
+        masRapida=item.fechaRealizacion - item.fechaCreacion;
+        idMasRapida=item.id;
+      }})
+      console.log(idMasRapida);
+      return idMasRapida;
+  }
+
   return (
     <>
         <input onClick={()=>borrarTodo()} className="enviar" type="button" value="Borrar todo" />
-        <input className="enviar" type="button" value="Calcular tarea" />
+        <input onClick={() => calcularTarea()} className="enviar" type="button" value="Calcular tarea" />
+        <p>{idMasRapida}</p>
     </>
   )
 }
